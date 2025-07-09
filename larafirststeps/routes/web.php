ccac3696fba8c634a15f3e8d691331d0f1774a81
Enrique Irazoqui Ruelas <enrique.irazoqui@carsol.mx>
2025-07-08 18:00:51 -0600

    primeras practicas

--- larafirststeps/routes/web.php
+++ larafirststeps/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/test', function(){
+//     return "Welcome";
+// });
+
+Route::get('/test', function(){
+    return view('test');
+});
+
+// Route::get('/crud', function(){
+//     return view('crud/index');
+// });
+
+Route::get('/crud', function(){
+
+    $age = 26;
+    $data = ['name' => 'Enrique', 'age' => $age];
+
+    return view('crud/index', $data);
+})->name('crud');
